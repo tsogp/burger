221429fcd2b19b0282a3a60aec57670d8094fbb6
ArsPro13 <131661233+ArsPro1323@users.noreply.github.com>
2023-11-10 11:28:09 +0300

    task1

---
+++ react-burger/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ABCD</h1>
+      </header>
+    </div>
+  );
+}
+
